1af7ad2db38a7ec5b2d70f3664e16739f376675e
BambaOir23 <92506036+BambaOir23@users.noreply.github.com>
2021-10-27 10:19:34 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void main(String[] args) {
+        AnimalFactory factory = new AnimalFactory();
+
+        Cat cat = factory.newCat(200);
+        Dog dog = factory.newDog(500, 10);
+
+        cat.runOn(125);
+        cat.swimOn(1);
+
+        dog.runOn(520);
+        dog.swimOn(9);
+
+
+        System.out.println("Животных создано всего: " + factory.getAnimalsCount());
+        System.out.println("Собак создано всего: " + factory.getDogsCount());
+        System.out.println("Кошек создано всего: " + factory.getCatsCount());
+    }
+}
