bf2be8926e9e0cf99debcd2ca10c4bed24ade3c2
BambaOir23 <92506036+BambaOir23@users.noreply.github.com>
2021-10-29 09:47:46 +0300

    Add files via upload

--- Cat.java
+++ Cat.java
@@ -1,10 +1,27 @@
-public class Cat extends Animal {
-        public Cat(float maxRunDistance) {
-            super(maxRunDistance, Integer.MIN_VALUE);
-        }
+public class Cat {
+    private boolean isFull;
+    private int appetite;
+    private String name;
+    private Person host;
 
-        @Override
-        public void swimOn(float requiredDistance) {
-            System.out.println("Кошки плавать не умеют!\n");
+    public Cat(int appetite, String name, Person host) {
+        isFull = false;
+        this.appetite = appetite;
+        this.name = name;
+        this.host = host;
+    }
+
+    public void eat(Plate plate) {
+        if (plate.hasEnoughFoodFor(appetite) && !isFull) {
+            System.out.println(name + " ест.");
+            plate.decreaseFood(appetite);
+            isFull = true;
+        } else if (!plate.hasEnoughFoodFor(appetite)) {
+            System.out.println(name + " жалобно теребит лапкой тарелку.");
+            host.addFoodTo(plate, appetite);
+        } else if (isFull) {
+            System.out.println(name + "сыт, неделю есть не будет");
         }
     }
+}
+
